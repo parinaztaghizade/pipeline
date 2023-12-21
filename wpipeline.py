c4b607639cfff2b8bec9a0ea4bc6b5b12f6bb537
parinaztaghizade <154323652+parinaztaghizade@users.noreply.github.com>
2023-12-21 23:56:08 +0100

    Update wpipeline.py

--- wpipeline.py
+++ wpipeline.py
@@ -1,16 +1,16 @@
+#Extract
+
 import openmeteo_requests
 
 import requests_cache
 import pandas as pd
 from retry_requests import retry
 
-# Setup the Open-Meteo API client with cache and retry on error
 cache_session = requests_cache.CachedSession('.cache', expire_after=-1)
 retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
 openmeteo = openmeteo_requests.Client(session=retry_session)
 
-# Make sure all required weather variables are listed here
-# The order of variables in hourly or daily is important to assign them correctly below
+
 url = "https://archive-api.open-meteo.com/v1/archive"
 params = {
     "latitude": 59.3294,
@@ -21,14 +21,12 @@ params = {
 }
 responses = openmeteo.weather_api(url, params=params)
 
-# Process first location. Add a for-loop for multiple locations or weather models
 response = responses[0]
 print(f"Coordinates {response.Latitude()}°E {response.Longitude()}°N")
 print(f"Elevation {response.Elevation()} m asl")
 print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
 print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
 
-# Process hourly data. The order of variables needs to be the same as requested.
 hourly = response.Hourly()
 hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
 hourly_rain = hourly.Variables(1).ValuesAsNumpy()
@@ -50,6 +48,7 @@ hourly_dataframe = pd.DataFrame(data=hourly_data)
 
 
 # Transform
+
 hourly_dataframe['year'] = hourly_dataframe['date'].dt.year
 hourly_dataframe['month'] = hourly_dataframe['date'].dt.month
 hourly_dataframe['day'] = hourly_dataframe['date'].dt.day
@@ -57,5 +56,7 @@ hourly_dataframe = pd.DataFrame(hourly_dataframe)
 hourly_dataframe['date'] = pd.to_datetime(hourly_dataframe['date'])
 hourly_dataframe['time'] = hourly_dataframe['date'].dt.strftime('%H:%M:%S')
 hourly_dataframe = hourly_dataframe.drop('date', axis=1)
+
 # Load
+
 hourly_dataframe.to_csv("Weather.csv")
